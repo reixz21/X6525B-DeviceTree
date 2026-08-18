af6d5c972a90bbc3f78738c9a5a7c2dbf30dff72
reixz <jimmuelrivera144@gmail.com>
2026-08-18 22:25:35 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X6525B-user
-add_lunch_combo omni_Infinix-X6525B-userdebug
-add_lunch_combo omni_Infinix-X6525B-eng
+add_lunch_combo twrp_Infinix-X6525B-user
+add_lunch_combo twrp_Infinix-X6525B-userdebug
+add_lunch_combo twrp_Infinix-X6525B-eng
